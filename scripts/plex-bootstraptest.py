866eb4dd4f91a352e4fd4e42006f3d09ffa4d6fe
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2023-10-14 23:09:36 -0400

    fix(action): improve network interface detection (#118)

--- scripts/plex-bootstraptest.py
+++ scripts/plex-bootstraptest.py
@@ -249,17 +249,18 @@ def setup_show(tvshows_path):
 
 def get_default_ip():
     """ Return the first IP address of the current machine if available. """
-    available_ips = list(
-        set(
-            [
-                i[4][0]
-                for i in socket.getaddrinfo(socket.gethostname(), None)
-                if i[4][0] not in ("127.0.0.1", "::1")
-                and not i[4][0].startswith("fe80:")
-            ]
-        )
-    )
-    return available_ips[0] if len(available_ips) else None
+    # https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.254.254.254', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
 
 
 def get_plex_account(opts):
