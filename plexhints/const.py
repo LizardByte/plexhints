e78148febc92a87c12855563be8097bf6179f4ff
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2023-11-30 18:17:45 -0500

    ci: move to rolling releases (#144)

--- plexhints/const.py
+++ plexhints/const.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
 """Constants used by plexhints."""
-
-# Library version
-MAJOR_VERSION = 0
-MINOR_VERSION = 1
-PATCH_VERSION = 3
-__short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
-__version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
-
 __name__ = 'plexhints'
 __description__ = 'Type hinting library for Plex plugin development.'
 
